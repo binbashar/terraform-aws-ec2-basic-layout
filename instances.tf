e1f9fc08321475f566642aa1790d975520f49aab
Diego OJ <diego.ojeda@binbash.com.ar>
2019-09-13 10:51:56 -0300

    Add support to specify an instance profile.

--- instances.tf
+++ instances.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "main" {
-    ami = var.ami_id
-    instance_type = var.instance_type
-    vpc_security_group_ids = [ aws_security_group.main.id ]
-    subnet_id = var.subnet_id
-    key_name = var.key_pair_name
+    ami                     = var.ami_id
+    instance_type           = var.instance_type
+    vpc_security_group_ids  = [ aws_security_group.main.id ]
+    subnet_id               = var.subnet_id
+    key_name                = var.key_pair_name
+    iam_instance_profile    = var.instance_profile
     
     root_block_device {
         volume_type = var.root_device_volume_type
