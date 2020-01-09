cf509d7a39002ddd0c06ab3b1803858bdd7a8ea9
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-01-09 12:41:16 -0300

    BBL-167 updating role name to be in sync with CircleCI ENV var.

--- examples/ec2-external-instance-profile/config.tf
+++ examples/ec2-external-instance-profile/config.tf
@@ -13,7 +13,7 @@ variable "region" {
 
 variable "profile" {
   description = "AWS Profile"
-  default     = "bb-shared-deploymaster"
+  default     = "bb-dev-deploymaster"
 }
 
 #=============================#
