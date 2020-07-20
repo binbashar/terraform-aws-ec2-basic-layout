e5b05de4259e2b211b73ff4a7786b7c4788b0742
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-07-20 13:27:53 -0300

    Std structure config propagation

--- versions.tf
+++ versions.tf
@@ -1,9 +1,9 @@
 
 terraform {
-  required_version = ">= 0.12.20"
+  required_version = ">= 0.12.24"
 
   required_providers {
-    aws   = ">= 2.46.0"
+    aws   = ">= 2.59.0"
     local = ">= 1.4.0"
   }
 }
